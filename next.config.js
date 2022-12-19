3275a871ef82526148e871b3db3c6f2d2918a2a7
Althafav <61247025+althafav@users.noreply.github.com>
2022-12-19 22:57:44 +0530

    data fetch all jobs working

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    API_URL: "http://localhost:8000"
-  }
-}
+  reactStrictMode: false,
+  env:{
+    API_URL: 'http://127.0.0.1:8000/', 
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Home from "../components/Home";
 import Layout from "../components/layout/Layout";
+import axios from "axios";
 
 
-export default function Index() {
+export default function Index({data}) {
+  console.log(data)
+
   return (
     <>
     <Layout>
@@ -11,4 +14,19 @@ export default function Index() {
       
     </>
   )
+}
+
+export async function getServerSideProps() {
+
+  const response = await axios.get(`${process.env.API_URL}api/jobs/`)
+
+  const data = response.data
+
+  return {
+    props: {
+      data,
+    }
+  }
+
+
 }
