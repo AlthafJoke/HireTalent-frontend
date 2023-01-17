288ea0748f8b3239dc406811810e815a5e15bfec
Althafav <61247025+althafav@users.noreply.github.com>
2023-01-17 20:23:20 +0530

    forgot password redirecting to new password is working

---
+++ pages/resetPassword/[uid]/[token].js
@@ -0,0 +1,32 @@
+import Layout from '../../../components/layout/Layout'
+import ResetPassword from '../../../components/auth/ResetPassword'
+
+import axios from 'axios'
+
+export default function VerifyPage() {
+  return (
+    <Layout >
+        <ResetPassword/> 
+    </Layout>
+  )
+}
+
+
+export async function getServerSideProps({ req, params }) {
+
+    const res = await axios.post(`${process.env.API_URL}api/forgot-password/verify/`,{
+      uid:params.uid,
+      token:params.token
+    })
+  
+    const data = res.data
+    
+
+  
+    return {
+      props: {
+        data,
+      },
+    }
+  
+  }
