75f44348d98d84fcffd29eb5ab9ca12ef66525e8
Althafav <61247025+althafav@users.noreply.github.com>
2023-01-18 01:07:11 +0530

    added scripts from layout to -document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 
 export default function Document() {
-  
-  
   return (
     <Html lang="en">
       <Head>
@@ -14,13 +12,34 @@ export default function Document() {
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossOrigin="anonymous"
         />
-        
       </Head>
       <body>
         <Main />
         <NextScript />
-        <script src="https://accounts.google.com/gsi/client" async defer></script>
+        <Script
+          strategy="beforeInteractive"
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        ></Script>
+        <Script
+          src="https://kit.fontawesome.com/9edb65c86a.js"
+          crossOrigin="anonymous"
+        ></Script>
 
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        ></Script>
+
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        ></Script>
+
+        <script
+          src="https://accounts.google.com/gsi/client"
+          async
+          defer
+        ></script>
       </body>
     </Html>
   );
