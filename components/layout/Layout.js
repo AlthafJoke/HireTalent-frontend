0681ab48703abcb197a2c9a6806eeebfc77dc30b
Althafav <61247025+althafav@users.noreply.github.com>
2023-01-18 11:10:38 +0530

    background style changed

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,27 +4,39 @@ import Script from "next/script";
 import Header from "./Header";
 import Footer from "./Footer";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = ({ children, title = "HireTalent - Find your job now" }) => {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        
       </Head>
+      <Script
+        strategy="beforeInteractive"
+        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      ></Script>
+      <Script
+        src="https://kit.fontawesome.com/9edb65c86a.js"
+        crossOrigin="anonymous"
+      ></Script>
 
-      
-      <ToastContainer position="top-right"/>
+      <Script
+        strategy="beforeInteractive"
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+      ></Script>
+
+      <Script
+        strategy="beforeInteractive"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+      ></Script>
+
+      <ToastContainer position="top-right" />
 
       <Header />
       {children}
 
-      <Footer/>
-
-
-
-
+      <Footer />
     </div>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -16,24 +15,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script
-          strategy="beforeInteractive"
-          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        ></Script>
-        <Script
-          src="https://kit.fontawesome.com/9edb65c86a.js"
-          crossOrigin="anonymous"
-        ></Script>
-
-        <Script
-          strategy="beforeInteractive"
-          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        ></Script>
-
-        <Script
-          strategy="beforeInteractive"
-          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-        ></Script>
+        
 
         <script
           src="https://accounts.google.com/gsi/client"
