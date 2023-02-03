11850f90d3d5f2537b62c47f911a47e1e02b4c2f
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-03 14:55:55 +0530

    dependencies added for useeffect

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -54,7 +54,7 @@ const Login = ({google_id}) => {
       theme: "outline",
       size: "large",
     });
-  },[]);
+  },[google_id, handleCallbackResponse]);
 
   useEffect(() => {
     if (error) {
@@ -76,7 +76,7 @@ const Login = ({google_id}) => {
     if (isAuthenticated && !loading) {
       Router.push("/");
     }
-  }, [clearErrors, repass, setSuccess]);
+  }, [clearErrors, repass, setSuccess, Router, error, isAuthenticated, loading, setRePass]);
 
   const submitHandler = (e) => {
     e.preventDefault();
