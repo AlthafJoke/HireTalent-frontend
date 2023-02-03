ad8c1af952ceb375e959d4ad4e8893f814229709
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-03 15:16:40 +0530

    test3

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main  } from "next/document";
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -14,15 +15,15 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <NextScript />
+       
         
 
-        <script
+        <Script
           src="https://accounts.google.com/gsi/client"
           async
           defer
-        ></script>
-        <script src="jwt-decode.js"></script>
+        ></Script>
+       
       </body>
     </Html>
   );
