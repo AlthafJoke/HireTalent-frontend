5fd8420d136ce84177693f3ce7d120ebe6ab37c8
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-07 12:26:24 +0530

    testting url

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -155,7 +155,7 @@ export const AuthProvider = ({ children }) => {
     try {
       setLoading(true);
 
-      const res = await axios.post(`${process.env.API_URL}api/auth/register/`, {
+      const res = await axios.post('https://hiretalent.codestreak.in/api/auth/register/', {
         first_name: firstName,
         last_name: lastName,
         email,
