0de85154b3d2bca0f21367eca878bfe111682fd6
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-08 16:06:44 +0530

    rm nextconfig from gitignore

---
+++ next.config.js
@@ -0,0 +1,13 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  env:{
+    API_URL: 'https://hiretalent.codestreak.in/',
+    GOOGLE_CLIENT: '692999485509-co0lqlgdf2vhpvd4jnc06vq6ji263tas.apps.googleusercontent.com',
+  },
+};
+
+module.exports = nextConfig;
+
