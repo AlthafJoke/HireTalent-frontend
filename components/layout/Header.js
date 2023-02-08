57f28062d6ed7b7caa47bd666701e52e002ff22b
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-08 13:17:44 +0530

    hosted url setup

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -68,7 +68,7 @@ const Header = () => {
     }
 
     const orderData = await axios.post(
-      "http://127.0.0.1:8000/api/createOrder/",
+      `${process.env.API_URL}api/createOrder/`,
       {
         amount: 199 * 100,
       }
@@ -92,7 +92,7 @@ const Header = () => {
         const amount = 199;
 
         const res = await axios.post(
-          "http://127.0.0.1:8000/api/verifySignature/",
+          `${process.env.API_URL}api/verifySignature/`,
           {
             razorpay_paymentId,
             razorpay_orderId,
