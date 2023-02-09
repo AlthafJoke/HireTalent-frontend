aef8abca370b91e52180dc2e4d76d32472cb2304
Althafav <61247025+althafav@users.noreply.github.com>
2023-02-09 15:11:32 +0530

    localhost:test:

--- pages/recruiter/index.js
+++ pages/recruiter/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import Chart from '../../components/Chart';
-import Dashboard from '../../components/recruiter/Dashboard';
 
 import Layout from '../../components/recruiter/layout/Layout';
-
 
 export default function RecruiterPage() {
   
     return (
       <>
       <Layout>
-        <Dashboard/>
         
       </Layout>
       </>
